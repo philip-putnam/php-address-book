99a4a10d24971b98218bca0fe85b057143685752
Philip Putnam <putnamp989@gmail.com>
2017-02-10 08:52:28 -0800

    session_start() and link home.html.twig in app.php

--- app/app.php
+++ app/app.php
@@ -3,10 +3,23 @@
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/Contact.php';
 
+
+    session_start();
+
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
     $app = new Silex\Application();
 
-    $app->get('/', function() {
-        return "hello world";
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+
+        return $app['twig']->render('home.html.twig');
+
     });
 
 
